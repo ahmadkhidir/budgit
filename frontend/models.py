6d22f19a52b50d33ceb1c30673e0c2aab85638aa
Ahmad Khidir <khidirahmad05@gmail.com>
2022-02-14 01:03:33 +0100

    CMS implemented

--- frontend/models.py
+++ frontend/models.py
@@ -1,10 +1,12 @@
-from tabnanny import verbose
 from django.db import models
 
 
 class Intro(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+
+    def __str__(self):
+        return "Intro"
 
     class Meta:
         verbose_name_plural = "intro"
@@ -13,6 +15,9 @@ class Intro(models.Model):
 class AboutUs(models.Model):
     heading = models.TextField()
     content = models.TextField()
+
+    def __str__(self):
+        return "About Us"
 
     class Meta:
         verbose_name_plural = "about us"
@@ -25,6 +30,9 @@ class WhatWeDo(models.Model):
 
     def get_image(self):
         return self.icon.url
+
+    def __str__(self):
+        return f"{self.title}"
 
     class Meta:
         verbose_name_plural = "what we do (s)"
@@ -39,6 +47,9 @@ class People(models.Model):
     def get_image(self):
         return self.image.url
 
+    def __str__(self):
+        return f"{self.name} ({self.role})"
+
     class Meta:
         verbose_name_plural = "peoples"
 
@@ -49,7 +60,8 @@ class Contact(models.Model):
     number = models.CharField(max_length=14)
     email = models.EmailField(max_length=254)
 
+    def __str__(self):
+        return "Contact"
 
     class Meta:
         verbose_name_plural = "contact"
-
