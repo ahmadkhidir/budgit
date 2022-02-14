4b98c237b27bb9ecb352e2df6c123d2fe93f069a
Ahmad Khidir <khidirahmad05@gmail.com>
2022-02-14 20:56:19 +0100

    Heroku implemented

--- budgit/settings.py
+++ budgit/settings.py
@@ -137,10 +137,10 @@ LANGUAGES = (
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
